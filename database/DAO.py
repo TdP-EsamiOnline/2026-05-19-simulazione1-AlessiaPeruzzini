c544655b990ff306d4c243d89192cffa1a6ea874
AlessiaPeruzzini <s327177@studenti.polito.it>
2026-06-01 10:45:12 +0200

    Simulazione 1

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,24 @@
 from database.DB_connect import DBConnect
+from model.genere import Genere
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllGenere():
+        conn = DBConnect.get_connection()
+
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select *
+            from genre"""
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(Genere(**row))
+
+        cursor.close()
+        conn.close()
+        return results
